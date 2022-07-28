2d504fe42524bd086c2a01fa60955d4c84c5fbc5
funddeveloper22 <openfund365@gmail.com>
2022-07-28 16:11:30 +0900

    swap hooks and transactions state

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,15 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query/react'
 import deposit from 'state/deposit/reducer'
 import user from 'state/user/reducer'
 import connection from 'state/connection/reducer'
+import transactions from 'state/transactions/reducer'
+import { updateVersion } from './global/actions'
 
 export const store = configureStore({
   reducer: {
   	deposit,
   	user,
-  	connection
+  	connection,
+  	transactions
   },
 })
+
+store.dispatch(updateVersion())
+
+setupListeners(store.dispatch)
+
 export default store
 
 
--- src/state/user/reducer.ts
+++ src/state/user/reducer.ts
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { ConnectionType } from 'connection'
+import { DEFAULT_DEADLINE_FROM_NOW } from '../../constants/misc'
+import { updateVersion } from '../global/actions'
+
+const currentTimestamp = () => new Date().getTime()
 
 export interface UserState {
   // We want the user to be able to define which wallet they want to use, even if there are multiple connected wallets via web3-react.
@@ -8,11 +12,18 @@ export interface UserState {
   // After some period of time, our active users will have this property set so we can likely remove the backfilling logic.
   selectedWalletBackfilled: boolean
   selectedWallet?: ConnectionType
+  // deadline set by user in minutes, used in all txns
+  userDeadline: number
+  timestamp: number
+  // the timestamp of the last updateVersion action
+  lastUpdateVersionTimestamp?: number
 }
 
 export const initialState: UserState = {
   selectedWallet: undefined,
   selectedWalletBackfilled: false,
+  userDeadline: DEFAULT_DEADLINE_FROM_NOW,
+  timestamp: currentTimestamp()
 }
 
 const userSlice = createSlice({
@@ -23,10 +34,32 @@ const userSlice = createSlice({
       state.selectedWallet = wallet
       state.selectedWalletBackfilled = true
     },
+    updateUserDeadline(state, action) {
+      state.userDeadline = action.payload.userDeadline
+      state.timestamp = currentTimestamp()
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(updateVersion, (state) => {
+      // deadline isnt being tracked in local storage, reset to default
+      // noinspection SuspiciousTypeOfGuard
+      if (
+        typeof state.userDeadline !== 'number' ||
+        !Number.isInteger(state.userDeadline) ||
+        state.userDeadline < 60 ||
+        state.userDeadline > 180 * 60
+      ) {
+        state.userDeadline = DEFAULT_DEADLINE_FROM_NOW
+      }
+
+      state.lastUpdateVersionTimestamp = currentTimestamp()
+    })
   },
 })
 
 export const {
   updateSelectedWallet,
+  updateUserDeadline
 } = userSlice.actions
 export default userSlice.reducer
