fe44d7d99f337d34f3d7d97889ba080db42ee0bd
fundDeveloper22 <openfund365@gmail.com>
2022-08-31 17:56:23 +0900

    Revert "update"
    This reverts commit b0a23b2b7fa3545b9e2c0485724bb04f17a61ca6.

--- src/hooks/useOnClickOutside.tsx
+++ src/hooks/useOnClickOutside.tsx
@@ -2,27 +2,18 @@ import { RefObject, useEffect, useRef } from 'react'
 
 export function useOnClickOutside<T extends HTMLElement>(
   node: RefObject<T | undefined>,
-  handler: undefined | (() => void),
-  ignoredNodes: Array<RefObject<T | undefined>> = []
+  handler: undefined | (() => void)
 ) {
   const handlerRef = useRef<undefined | (() => void)>(handler)
-
   useEffect(() => {
     handlerRef.current = handler
   }, [handler])
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
-      const nodeClicked = node.current?.contains(e.target as Node)
-      const ignoredNodeClicked = ignoredNodes.reduce(
-        (reducer, val) => reducer || !!val.current?.contains(e.target as Node),
-        false
-      )
-
-      if ((nodeClicked || ignoredNodeClicked) ?? false) {
+      if (node.current?.contains(e.target as Node) ?? false) {
         return
       }
-
       if (handlerRef.current) handlerRef.current()
     }
 
@@ -31,5 +22,5 @@ export function useOnClickOutside<T extends HTMLElement>(
     return () => {
       document.removeEventListener('mousedown', handleClickOutside)
     }
-  }, [node, ignoredNodes])
+  }, [node])
 }
