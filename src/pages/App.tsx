bd480f2691e7c81d6a0eb6dc00507362ed4ad28c
fundDeveloper22 <openfund365@gmail.com>
2022-09-01 07:15:57 +0900

    update remain error
    src/nft/components/icons.tsx
      Line 290:19:   'fill' is missing in props validation  react/prop-types
      Line 301:19:   'fill' is missing in props validation  react/prop-types
      Line 1347:19:  'fill' is missing in props validation  react/prop-types
      Line 1347:32:  'fill' is missing in props validation  react/prop-types

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,33 +1,214 @@
-import './App.css'
-
-import ResponsiveAppBar from 'pages/Appbar'
-import CreateFund from 'pages/CreateFund'
+import { initializeAnalytics, sendAnalyticsEvent, user } from 'components/AmplitudeAnalytics'
+import { CUSTOM_USER_PROPERTIES, EventName, PageName } from 'components/AmplitudeAnalytics/constants'
+import { Trace } from 'components/AmplitudeAnalytics/Trace'
+import Loader from 'components/Loader'
+import TopLevelModals from 'components/TopLevelModals'
+import { useFeatureFlagsIsLoaded } from 'featureFlags'
+import { NavBarVariant, useNavBarFlag } from 'featureFlags/flags/navBar'
+import { useNftFlag } from 'featureFlags/flags/nft'
+import { useTokensFlag } from 'featureFlags/flags/tokens'
+import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
+//import Swap from './Swap'
+//import CreateFund from 'pages/CreateFund'
 import Fund from 'pages/Fund'
-import FundDeposit from 'pages/Fund/FundDetail/deposit'
-import FundSwap from 'pages/Fund/FundDetail/swap/swap'
-import FundWithdraw from 'pages/Fund/FundDetail/withdraw'
+import FundList from 'pages/Fund'
+import Deposit from 'pages/Fund/FundDetail/deposit'
+import Swap from 'pages/Fund/FundDetail/swap/swap'
+import Withdraw from 'pages/Fund/FundDetail/withdraw'
 import Main from 'pages/Main'
-import OverView from 'pages/OverView'
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import Overview from 'pages/OverView'
+import { lazy, Suspense, useEffect } from 'react'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { useIsDarkMode } from 'state/user/hooks'
+import styled from 'styled-components/macro'
+import { SpinnerSVG } from 'theme/components'
+import { getBrowser } from 'utils/browser'
+import { getCLS, getFCP, getFID, getLCP, Metric } from 'web-vitals'
 
-function App() {
+import { useAnalyticsReporter } from '../components/analytics'
+import ErrorBoundary from '../components/ErrorBoundary'
+import Header from '../components/Header'
+import Polling from '../components/Header/Polling'
+import Navbar from '../components/NavBar'
+import Popups from '../components/Popups'
+import { useIsExpertMode } from '../state/user/hooks'
+import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
+
+const Vote = lazy(() => import('pages/Fund/FundDetail/swap/swap'))
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+`
+
+const BodyWrapper = styled.div<{ navBarFlag: NavBarVariant }>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: ${({ navBarFlag }) => (navBarFlag === NavBarVariant.Enabled ? `72px 0px 0px 0px` : `120px 0px 0px 0px`)};
+  align-items: center;
+  flex: 1;
+  ${({ theme }) => theme.deprecated_mediaWidth.deprecated_upToSmall`
+    padding: 52px 0px 16px 0px;
+  `};
+`
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+`
+
+const Marginer = styled.div`
+  margin-top: 5rem;
+`
+
+function getCurrentPageFromLocation(locationPathname: string): PageName | undefined {
+  switch (locationPathname) {
+    case '/swap':
+      return PageName.SWAP_PAGE
+    case '/vote':
+      return PageName.VOTE_PAGE
+    case '/pool':
+      return PageName.POOL_PAGE
+    case '/tokens':
+      return PageName.TOKENS_PAGE
+    default:
+      return undefined
+  }
+}
+
+// this is the same svg defined in assets/images/blue-loader.svg
+// it is defined here because the remote asset may not have had time to load when this file is executing
+const LazyLoadSpinner = () => (
+  <SpinnerSVG width="94" height="94" viewBox="0 0 94 94" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M92 47C92 22.1472 71.8528 2 47 2C22.1472 2 2 22.1472 2 47C2 71.8528 22.1472 92 47 92"
+      stroke="#2172E5"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </SpinnerSVG>
+)
+
+export default function App() {
+  const isLoaded = useFeatureFlagsIsLoaded()
+  const tokensFlag = useTokensFlag()
+  const navBarFlag = useNavBarFlag()
+  const nftFlag = useNftFlag()
+
+  const { pathname } = useLocation()
+  const currentPage = getCurrentPageFromLocation(pathname)
+  const isDarkMode = useIsDarkMode()
+  const isExpertMode = useIsExpertMode()
+
+  useAnalyticsReporter()
+  initializeAnalytics()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  useEffect(() => {
+    sendAnalyticsEvent(EventName.APP_LOADED)
+    user.set(CUSTOM_USER_PROPERTIES.BROWSER, getBrowser())
+    user.set(CUSTOM_USER_PROPERTIES.SCREEN_RESOLUTION_HEIGHT, window.screen.height)
+    user.set(CUSTOM_USER_PROPERTIES.SCREEN_RESOLUTION_WIDTH, window.screen.width)
+    getCLS(({ delta, id }: Metric) => sendAnalyticsEvent(EventName.WEB_VITALS, { cumulative_layout_shift: delta }))
+    getFCP(({ delta, id }: Metric) => sendAnalyticsEvent(EventName.WEB_VITALS, { first_contentful_paint_ms: delta }))
+    getFID(({ delta, id }: Metric) => sendAnalyticsEvent(EventName.WEB_VITALS, { first_input_delay_ms: delta }))
+    getLCP(({ delta, id }: Metric) => sendAnalyticsEvent(EventName.WEB_VITALS, { largest_contentful_paint_ms: delta }))
+  }, [])
+
+  useEffect(() => {
+    user.set(CUSTOM_USER_PROPERTIES.DARK_MODE, isDarkMode)
+  }, [isDarkMode])
+
+  useEffect(() => {
+    user.set(CUSTOM_USER_PROPERTIES.EXPERT_MODE, isExpertMode)
+  }, [isExpertMode])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <ResponsiveAppBar />
-      </header>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/overview" element={<OverView />} />
-        <Route path="/createFund" element={<CreateFund />} />
-        <Route path="/fund" element={<Fund />} />
-        <Route path="/deposit" element={<FundDeposit />} />
-        <Route path="/withdraw" element={<FundWithdraw />} />
-        <Route path="/swap" element={<FundSwap />} />
-      </Routes>
-    </div>
+    <ErrorBoundary>
+      <DarkModeQueryParamReader />
+      <ApeModeQueryParamReader />
+      <AppWrapper>
+        <Trace page={currentPage}>
+          <HeaderWrapper>{navBarFlag === NavBarVariant.Enabled ? <Navbar /> : <Header />}</HeaderWrapper>
+          <BodyWrapper navBarFlag={navBarFlag}>
+            <Popups />
+            <Polling />
+            <TopLevelModals />
+            <Suspense fallback={<Loader />}>
+              {isLoaded ? (
+                <Routes>
+                  <Route path="/" element={<Main />} />
+                  <Route path="/overview" element={<Overview />} />
+                  <Route path="/fund" element={<FundList />} />
+                  <Route path="/fund/:fundID" element={<Fund />} />
+                  <Route path="deposit/:fund/:tokenId" element={<Deposit />} />
+                  <Route path="withdraw/:fund/:tokenId" element={<Withdraw />} />
+                  <Route path="swap/:fund/:currencyIdA/:currencyIdB" element={<Swap />} />
+                  <Route
+                    path="vote/*"
+                    element={
+                      <Suspense fallback={<LazyLoadSpinner />}>
+                        <Vote />
+                      </Suspense>
+                    }
+                  />
+                  <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} />
+                  <Route path="earn" element={<Swap />} />
+
+                  <Route path="*" element={<Overview />} />
+                </Routes>
+              ) : (
+                <Loader />
+              )}
+            </Suspense>
+            <Marginer />
+          </BodyWrapper>
+        </Trace>
+      </AppWrapper>
+    </ErrorBoundary>
   )
 }
 
-export default App
+// import './App.css'
+
+// import ResponsiveAppBar from 'pages/Appbar'
+// import CreateFund from 'pages/CreateFund'
+// import Fund from 'pages/Fund'
+// import FundDeposit from 'pages/Fund/FundDetail/deposit'
+// import FundSwap from 'pages/Fund/FundDetail/swap/swap'
+// import FundWithdraw from 'pages/Fund/FundDetail/withdraw'
+// import Main from 'pages/Main'
+// import OverView from 'pages/OverView'
+// import React from 'react'
+// import { Route, Routes } from 'react-router-dom'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <ResponsiveAppBar />
+//       </header>
+//       <Routes>
+//         <Route path="/" element={<Main />} />
+//         <Route path="/overview" element={<OverView />} />
+//         <Route path="/createFund" element={<CreateFund />} />
+//         <Route path="/fund" element={<Fund />} />
+//         <Route path="/deposit" element={<FundDeposit />} />
+//         <Route path="/withdraw" element={<FundWithdraw />} />
+//         <Route path="/swap" element={<FundSwap />} />
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App
