d8bc31711515836328d540320975845d5800af13
fundDeveloper22 <openfund365@gmail.com>
2023-07-03 00:07:40 +0900

    fix build fail
    remove unused value

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,9 +6,9 @@ export function isTestEnv(): boolean {
   return process.env.NODE_ENV === 'test'
 }
 
-// export function isStagingEnv(): boolean {
-//   return Boolean(process.env.REACT_APP_STAGING)
-// }
+function isStagingEnv(): boolean {
+  return Boolean(process.env.REACT_APP_STAGING)
+}
 
 export function isProductionEnv(): boolean {
   return process.env.NODE_ENV === 'production' && !isStagingEnv()
