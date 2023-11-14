3c0da03444cf7e6a1b16f8b53bca6fe8105d86e0
fundDeveloper22 <openfund365@gmail.com>
2023-11-14 16:32:40 +0900

    update home page mobile UI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Trans } from '@lingui/macro'
 import { useWeb3React } from '@web3-react/core'
 import dotoliMainLogo from 'assets/images/dotoli_main_logo.png'
-import { RowBetween } from 'components/Row'
+import { AutoColumn } from 'components/Column'
 import { VOTE_URL } from 'constants/addresses'
 import { isSupportedChain } from 'constants/chains'
 import { ErrorContainer, NetworkIcon } from 'pages/Account'
@@ -22,6 +22,10 @@ import {
   CoverH1,
   CoverP,
 } from './CoverElements'
+
+const PageWrapper = styled.div`
+  width: 90%;
+`
 
 const Button = styled(RebassButton)<{ primary?: boolean; big?: boolean; dark?: boolean; fontBig?: boolean }>`
   border-radius: 50px;
@@ -51,124 +55,117 @@ export default function Home() {
   const [hover, setHover] = useState(false)
 
   return (
-    <div>
+    <PageWrapper>
       <center>
         <img src={dotoliMainLogo} width="400px" />
       </center>
       {isSupportedChain(chainId) ? (
         <>
-          <CoverContainer>
-            <CoverContent>
-              <CoverH0>
-                <LinearGradient gradient={['to left', '#F8FFC3 ,#ff9933']}>
-                  <Trans>Decentralized cryptocurrency fund</Trans>
-                </LinearGradient>
-              </CoverH0>
-              <br></br>
-              <br></br>
-              <CoverBtnWrapper>
-                <Button
-                  primary={true}
-                  big={true}
-                  dark={true}
-                  fontBig={true}
-                  marginRight="15px"
-                  onClick={() => navigate(`/account`)}
-                >
-                  <Trans>Get Started</Trans> {hover ? <ArrowForward /> : <ArrowRight />}
-                </Button>
-              </CoverBtnWrapper>
-            </CoverContent>
-          </CoverContainer>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <CoverContainer>
-            <CoverContent>
-              <CoverH1>
-                <Trans>More benefits for investors and fund managers</Trans>
-              </CoverH1>
-              <CoverP>
-                <Trans>
-                  Since the investor invests directly to the fund manager, the fee is low, and the fund manager get
-                  rewards in proportion to the investment performance
-                </Trans>
-              </CoverP>
-            </CoverContent>
-          </CoverContainer>
-          <br></br>
-          <br></br>
-          <CoverContainer>
-            <CoverContent>
-              <CoverH1>
-                <Trans>Decentralized fund investment</Trans>
-              </CoverH1>
-              <CoverP>
-                <Trans>
-                  Only investors can withdraw their own funds. Fund managers can only swap token or manage pool via
-                  Dotoli and Uniswap
-                </Trans>
-              </CoverP>
-            </CoverContent>
-          </CoverContainer>
-          <br></br>
-          <br></br>
-          <CoverContainer>
-            <CoverContent>
-              <CoverH1>
-                <Trans>Transparent fund management</Trans>
-              </CoverH1>
-              <CoverP>
-                <Trans>
-                  All investments made by the fund manager are recorded. Investors can find great fund managers. Also,
-                  fund managers can attract investors through investment performance.
-                </Trans>
-              </CoverP>
-            </CoverContent>
-          </CoverContainer>
-          <br></br>
-          <br></br>
-          <CoverContainer>
-            <CoverContent>
-              <CoverH1>
-                <Trans>Governance</Trans>
-              </CoverH1>
-              <CoverP>
-                <Trans>
-                  Fee rate and investable cryptos are determined by voting. You can vote with governance token and
-                  increase governance token by staking
-                </Trans>
-              </CoverP>
-
-              <CoverBtnWrapper>
-                <RowBetween>
+          <AutoColumn>
+            <CoverContainer>
+              <CoverContent>
+                <CoverH0>
+                  <LinearGradient gradient={['to left', '#F8FFC3 ,#ff9933']}>
+                    <Trans>Decentralized cryptocurrency fund</Trans>
+                  </LinearGradient>
+                </CoverH0>
+                <CoverBtnWrapper>
                   <Button
                     primary={true}
                     big={true}
                     dark={true}
                     fontBig={true}
                     marginRight="15px"
-                    onClick={() => navigate(`/staking`)}
+                    onClick={() => navigate(`/account`)}
                   >
-                    <Trans>Staking</Trans> {hover ? <ArrowForward /> : <ArrowRight />}
+                    <Trans>Get Started</Trans> {hover ? <ArrowForward /> : <ArrowRight />}
                   </Button>
-                  <Button
-                    primary={true}
-                    big={true}
-                    dark={true}
-                    fontBig={true}
-                    onClick={() => {
-                      window.open(VOTE_URL)
-                    }}
-                  >
-                    <Trans>Governance</Trans> {hover ? <ArrowForward /> : <ArrowRight />}
-                  </Button>
-                </RowBetween>
-              </CoverBtnWrapper>
-            </CoverContent>
-          </CoverContainer>
+                </CoverBtnWrapper>
+              </CoverContent>
+            </CoverContainer>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <CoverContainer>
+              <CoverContent>
+                <CoverH1>
+                  <Trans>More benefits for investors and fund managers</Trans>
+                </CoverH1>
+                <CoverP>
+                  <Trans>
+                    Since the investor invests directly to the fund manager, the fee is low, and the fund manager get
+                    rewards in proportion to the investment performance
+                  </Trans>
+                </CoverP>
+              </CoverContent>
+            </CoverContainer>
+            <br></br>
+            <br></br>
+            <CoverContainer>
+              <CoverContent>
+                <CoverH1>
+                  <Trans>Decentralized fund investment</Trans>
+                </CoverH1>
+                <CoverP>
+                  <Trans>
+                    Only investors can withdraw their own funds. Fund managers can only swap token or manage pool via
+                    Dotoli and Uniswap
+                  </Trans>
+                </CoverP>
+              </CoverContent>
+            </CoverContainer>
+            <br></br>
+            <br></br>
+            <CoverContainer>
+              <CoverContent>
+                <CoverH1>
+                  <Trans>Transparent fund management</Trans>
+                </CoverH1>
+                <CoverP>
+                  <Trans>
+                    All investments made by the fund manager are recorded. Investors can find great fund managers. Also,
+                    fund managers can attract investors through investment performance.
+                  </Trans>
+                </CoverP>
+              </CoverContent>
+            </CoverContainer>
+            <br></br>
+            <br></br>
+            <CoverContainer>
+              <CoverContent>
+                <CoverH1>
+                  <Trans>Governance</Trans>
+                </CoverH1>
+                <CoverP>
+                  <Trans>
+                    Fee rate and investable cryptos are determined by voting. You can vote with governance token and
+                    increase governance token by staking
+                  </Trans>
+                </CoverP>
+                <AutoColumn gap="4px">
+                  <CoverBtnWrapper>
+                    <Button primary={true} big={true} dark={true} fontBig={true} onClick={() => navigate(`/staking`)}>
+                      <Trans>Staking</Trans> {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                  </CoverBtnWrapper>
+                  <CoverBtnWrapper>
+                    <Button
+                      primary={true}
+                      big={true}
+                      dark={true}
+                      fontBig={true}
+                      onClick={() => {
+                        window.open(VOTE_URL)
+                      }}
+                    >
+                      <Trans>Governance</Trans> {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                  </CoverBtnWrapper>
+                </AutoColumn>
+              </CoverContent>
+            </CoverContainer>
+          </AutoColumn>
         </>
       ) : chainId !== undefined ? (
         <ErrorContainer>
@@ -180,6 +177,6 @@ export default function Home() {
           </ThemedText.DeprecatedBody>
         </ErrorContainer>
       ) : null}
-    </div>
+    </PageWrapper>
   )
 }
